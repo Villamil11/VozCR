1a8a1a3d3ec37476c2853990d978ebaea9da47fa
tan25gonz-ux <tan25gonz@gmail.com>
2026-07-10 16:20:58 -0600

    Update Reportar.php

--- Reportar.php
+++ Reportar.php
@@ -37,55 +37,41 @@
 
     <h2>Formulario de Reporte</h2>
 
-    <form method="POST">
-
+    <form metho="POST">
         <p>
             <label>Nombre completo</label><br>
             <input type="text" name="nombre" required>
         </p>
-
         <p>
             <label>Correo electrónico</label><br>
             <input type="email" name="correo" required>
         </p>
-
         <p>
             <label>Categoría</label><br>
-
             <select name="categoria">
-
                 <option>Huecos en carretera</option>
                 <option>Basura acumulada</option>
                 <option>Alumbrado público</option>
                 <option>Seguridad</option>
                 <option>Daños en infraestructura</option>
-
             </select>
-
         </p>
-
         <p>
             <label>Ubicación</label><br>
             <input type="text" name="ubicacion" required>
         </p>
-
         <p>
             <label>Descripción del incidente</label><br>
-
             <textarea
                 name="descripcion"
                 rows="6"
                 required></textarea>
-
         </p>
-
         <p>
             <label>Fotografía (Opcional)</label><br>
             <input type="file" name="imagen">
         </p>
-
         <br>
-
         <input type="submit" value="Enviar Reporte" class="btn">
 
     </form>
@@ -93,9 +79,7 @@
 </section>
 
 <?php
-
 if(isset($_POST["nombre"])){
-
     $nombre = $_POST["nombre"];
     $correo = $_POST["correo"];
     $categoria = $_POST["categoria"];
@@ -103,40 +87,20 @@ if(isset($_POST["nombre"])){
     $descripcion = $_POST["descripcion"];
 
 ?>
-
 <section class="cards">
-
     <h2>Reporte recibido</h2>
-
     <div class="card">
-
         <h3>Información enviada</h3>
-
         <p><strong>Nombre:</strong> <?php echo $nombre; ?></p>
-
         <p><strong>Correo:</strong> <?php echo $correo; ?></p>
-
         <p><strong>Categoría:</strong> <?php echo $categoria; ?></p>
-
         <p><strong>Ubicación:</strong> <?php echo $ubicacion; ?></p>
-
         <p><strong>Descripción:</strong> <?php echo $descripcion; ?></p>
-
         <p><strong>Estado:</strong> Pendiente de revisión.</p>
-
     </div>
-
 </section>
-
-<?php
-}
-?>
-
 <footer>
-
     <p>VozCR © 2026 | Proyecto Universitario - Universidad Fidélitas</p>
-
 </footer>
-
 </body>
 </html>
